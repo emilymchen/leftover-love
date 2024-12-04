3a44b171ec893d4041bb107ea5b0606a578ccb3c
Emily Chen <emichen@mit.edu>
2024-12-04 14:20:27 -0500

    login style improvements

--- client/components/Login/LoginForm.vue
+++ client/components/Login/LoginForm.vue
@@ -15,8 +15,7 @@ async function login() {
 </script>
 
 <template>
-  <form class="pure-form pure-form-aligned" @submit.prevent="login">
-    <h3>Login</h3>
+  <form class="pure-form pure-form-aligned login" @submit.prevent="login">
     <fieldset>
       <div class="pure-control-group">
         <label for="aligned-name">Username</label>
@@ -31,17 +30,54 @@ async function login() {
       </div>
     </fieldset>
   </form>
+  <div class="register-section">
+    <h3>
+      No account yet? <RouterLink to="/register-donor" class="register-link">Register as a restaurant</RouterLink>,
+      <RouterLink to="/register-recipient" class="register-link">register as a recipient</RouterLink>, or
+      <RouterLink to="/register-volunteer" class="register-link">register as a volunteer</RouterLink>.
+    </h3>
+  </div>
 </template>
 
 <style scoped>
-h3 {
+.pure-control-group {
   display: flex;
-  justify-content: center;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 1rem;
+  margin-bottom: 1rem;
 }
 
 .pure-button-primary {
-  background-color: var(--red); 
-  border-color: var(--red); 
-  color: white; 
+  margin-left: -30px;
+  background-color: #d23818;
+  color: #f7bfa8;
+  border: none;
+  border-radius: 100px;
+  padding: 0.8rem 2rem;
+  font-size: 1.3rem;
+}
+
+.pure-button-primary:hover {
+  background-color: #bf2000;
+}
+
+.register-section {
+  text-align: center;
+  margin-top: 1.5rem;
+  font-size: 1rem;
+  color: #194326;
+  width: 30vw;
+}
+
+.register-link {
+  color: #43603b;
+  text-decoration: underline;
+  font-weight: bold;
+}
+
+.register-link:hover {
+  color: #2f4d2e;
+  text-decoration: none;
 }
 </style>
--- client/views/LoginView.vue
+++ client/views/LoginView.vue
@@ -4,7 +4,23 @@ import LoginForm from "@/components/Login/LoginForm.vue";
 
 <template>
   <main class="column">
-    <h1>Leftover Love</h1>
+    <h1>Log In</h1>
     <LoginForm />
   </main>
 </template>
+
+<style scoped>
+h1 {
+  color: #d23818;
+  font-size: 40pt;
+  font-weight: lighter;
+  margin-bottom: 5vh;
+}
+
+.column {
+  background-image: url("@/assets/images/landing-background.png");
+  background-size: cover;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
